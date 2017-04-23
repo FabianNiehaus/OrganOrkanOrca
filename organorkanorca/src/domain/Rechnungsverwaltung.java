3cf437a5b51cd6222ed2530baef4136d87ae9106
Fabian Niehaus <fabian.niehaus.94@gmail.com>
2017-04-23 16:32:10 +0200

    Kommentare Rechnungsverwaltung

--- organorkanorca/src/domain/Rechnungsverwaltung.java
+++ organorkanorca/src/domain/Rechnungsverwaltung.java
@@ -7,10 +7,22 @@ import data_objects.Kunde;
 import data_objects.Rechnung;
 import data_objects.Warenkorb;
 
+/**
+ * @author Fabian Niehaus
+ * Klasse zur Verwaltung von Rechnungen
+ */
 public class Rechnungsverwaltung {
 	
 	private Vector<Rechnung> rechnungen = new Vector<Rechnung>();
 	
+	/**
+	 * Erzeugt eine neue Rechung
+	 * @param ku Zugehöriger Kunde
+	 * @param datum Rechnungsdatum
+	 * @param wk Zugehöriger Warenkorb
+	 * @param gesamt Gesamtsumme der Rechnung
+	 * @return Erzeugte Rechnung
+	 */
 	public Rechnung rechnungErzeugen(Kunde ku, Date datum, Warenkorb wk, int gesamt){
 		Rechnung re = new Rechnung(ku, datum, wk, gesamt);
 		rechnungen.add(re);
