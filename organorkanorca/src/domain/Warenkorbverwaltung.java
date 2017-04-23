7010dcb89b35956a2a7e9ab3be697354d16f7993
Fabian Niehaus <fabian.niehaus.94@gmail.com>
2017-04-23 16:36:59 +0200

    Kommentare Warenkorbverwaltung

--- organorkanorca/src/domain/Warenkorbverwaltung.java
+++ organorkanorca/src/domain/Warenkorbverwaltung.java
@@ -4,10 +4,19 @@ import java.util.LinkedHashMap;
 import java.util.Vector;
 import data_objects.*;
 
+/**
+ * @author Fabian Niehaus
+ * Klasse zur Verwaltung von Warenkörben
+ */
 public class Warenkorbverwaltung {
 	
 	private Vector<Warenkorb> warenkoerbe = new Vector<>();
 
+	/**
+	 * Gibt einen Warenkorb aus, sofern dieser in der Verwaltung exisitert
+	 * @param wk Gesuchter Warenkorb
+	 * @return Gesuchter Warenkorb
+	 */
 	public Warenkorb getWarenkorb(Warenkorb wk){
 		for(Warenkorb ret : warenkoerbe){
 			if(ret.equals(wk)){
@@ -17,12 +26,20 @@ public class Warenkorbverwaltung {
 		return null;
 	}
 	
+	/**
+	 * Erzeugt einen neuen (leeren) Warenkorb
+	 * @return Erzeugter Warenkorb
+	 */
 	public Warenkorb erstelleWarenkorb(){
 		Warenkorb wk = new Warenkorb();
 		warenkoerbe.addElement(wk);
 		return wk;
 	}
 	
+	/**
+	 * Löscht einen Warenkorb
+	 * @param wk Zu löschender Warenkorb
+	 */
 	public void loescheWarenkorb(Warenkorb wk){
 		for(Warenkorb del : warenkoerbe){
 			if(del.equals(wk)){
@@ -31,23 +48,47 @@ public class Warenkorbverwaltung {
 		}
 	}
 	
+	/**
+	 * Ändert die Anzahl eines Artikels in einem Warenkorb. Die Artikelauswahl erfolgt nach Position.
+	 * @param aend Zu bearbeitender Warenkorb
+	 * @param position Position des zu verändernden Artikels
+	 * @param anz Neue Anzahl
+	 */
 	public void aendereWarenkorb(Warenkorb aend, int position, int anz){
 		Warenkorb wk = getWarenkorb(aend);
 		wk.aendereAnzahl(position, anz);
 	}
 	
+	/**
+	 * Fügt einem Warenkorb einen Artikel hinzu
+	 * @param wk Zu bearbeitender Warenkorb
+	 * @param art Hinzuzufügender Artikel
+	 * @param anz Anzahl des Artikels
+	 */
 	public void legeInWarenkorb(Warenkorb wk, Artikel art, int anz){
 		wk.speichereArtikel(art, anz);
 	}
 	
+	/**
+	 * @return Alle Warenkörbe
+	 */
 	public Vector<Warenkorb> getWarenkoerbe() {
 		return warenkoerbe;
 	}
 	
+	/**
+	 * Leert einen Warenkorb
+	 * @param wk Zu leerender Warenkorb
+	 */
 	public void leereWarenkorb(Warenkorb wk){
 		wk.leereWarkenkorb();
 	}
 	
+	/**
+	 * Gibt den Inhalt eines Warenkorbs aus
+	 * @param wk Gewünschter Warenkorb
+	 * @return Alle Artikel mit Anzahl
+	 */
 	public LinkedHashMap<Artikel, Integer> getArtikel(Warenkorb wk){
 		return wk.getArtikel();
 	}
