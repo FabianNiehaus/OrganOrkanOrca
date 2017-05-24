d934a7ee7a7c999878371432d3cf8693e6b6e23f
Fabian <fabian.niehaus.94@gmail.com>
2017-05-24 18:33:47 +0200

    a

--- organorkanorca/src/user/GUI.java
+++ organorkanorca/src/user/GUI.java
@@ -1,12 +1,19 @@
 package user;
 
 import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
 
 import data_objects.Artikel;
+import data_objects.Kunde;
+import data_objects.Massengutartikel;
+import data_objects.Mitarbeiter;
 import data_objects.Person;
 import domain.eShopCore;
 import domain.exceptions.AccessRestrictedException;
+import domain.exceptions.ArticleNonexistantException;
 import domain.exceptions.LoginFailedException;
+import util.IO;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -62,7 +69,12 @@ public class GUI extends JFrame {
 		moduleButtons.add(shopButton);
 		
 		leftArea.add(moduleButtons);
-		leftArea.add(new Artikelsichtfenster());
+		try {
+			leftArea.add(new Artikelsichtfenster());
+		} catch (AccessRestrictedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		rightArea.add(new Warenkorbverwaltungsfenster());
 
 		main.setDividerLocation((int)(this.getWidth()*0.60));
@@ -73,7 +85,7 @@ public class GUI extends JFrame {
 		this.setVisible(true);
 	}
 		
-	class Artikelsichtfenster extends JPanel{
+	abstract class Sichtfenster extends JPanel{
 		
 		JPanel overviewButtons = new JPanel();
 		JButton alleButton = new JButton("Alle");
@@ -83,17 +95,19 @@ public class GUI extends JFrame {
 		JTextField sucheField = new JTextField();
 		
 		JPanel leftAreaActionField = new JPanel();
-		JTextArea auflistung = new JTextArea();
-		JButton aktion = new JButton("In Warenkorb");
+		JTable auflistung;
+		JButton aktion = new JButton();
 		JTextField anzahl = new JTextField();
 		
-		public Artikelsichtfenster(){
+		public Sichtfenster(AbstractTableModel atm, String aktionsname, boolean anzahlZeigen){
 			
 			this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 			this.setPreferredSize(new Dimension((int)(GUI.this.getWidth()*0.55), (int)(GUI.this.getHeight()*0.8)));
 			
 			overviewButtons.setMaximumSize(new Dimension(1024, 40));
-			
+						
+			auflistung = new JTable(atm);
+			auflistung.setAutoCreateRowSorter(true);
 			
 			this.add(overviewButtons);
 			this.add(auflistung);
@@ -110,148 +124,255 @@ public class GUI extends JFrame {
 			sucheButton.addActionListener(new ButtonActionListener());
 			overviewButtons.add(sucheButton);
 			overviewButtons.setVisible(true);
-			
-			auflistung.setEditable(false);
-			try {
-				auflistung.setText("");
-				for (Artikel art : eShop.alleArtikelAusgeben(user)){
-					auflistung.append(art.toString() + "\n");
-				}
-			} catch (AccessRestrictedException e) {
-				auflistung.setText(e.getMessage());
-			}
+					
+			aktion.setText(aktionsname);
 			
 			leftAreaActionField.add(aktion);
-			leftAreaActionField.add(anzahl);
+			if(anzahlZeigen){
+				leftAreaActionField.add(anzahl);
+			}
 		}
+		
+		public abstract void auflistungInitialize();
+		
+		public abstract void sortiereNummer();
+		
+		public abstract void sortiereBezeichnung();
+		
+		public abstract void suche(String suchStr);
 		
 		class ButtonActionListener implements ActionListener {
 
 			@Override
 			public void actionPerformed(ActionEvent ae) {
 				if (ae.getSource().equals(alleButton) || ae.getSource().equals(sortNrButton)){
-					Vector<Artikel> artSort;
-					try {
-						artSort = eShop.alleArtikelAusgeben(user);
-						//Sortieren nach Artikelnummer
-						Collections.sort(artSort, 
-							(Artikel o1, Artikel o2) -> o1.getArtikelnummer() - o2.getArtikelnummer());
-						auflistung.setText("");
-						for (Artikel art : artSort){
-							auflistung.append(art.toString() + "\n");
-						}
-					} catch (AccessRestrictedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					sortiereNummer();
 				} else if (ae.getSource().equals(sortBezButton)){
-					Vector<Artikel> artSort;
-					try {
-						artSort = eShop.alleArtikelAusgeben(user);
-						//Sortieren nach Artikelnummer
-						Collections.sort(artSort, 
-							(Artikel o1, Artikel o2) -> o1.getBezeichnung().compareTo(o2.getBezeichnung()));
-						auflistung.setText("");
-						for (Artikel art : artSort){
-							auflistung.append(art.toString() + "\n");
-						}
-					} catch (AccessRestrictedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					sortiereBezeichnung();
+				} else if (ae.getSource().equals(sucheButton)){
+					suche(sucheField.getText());
 				}
 			}
 			
 		}
 	}
 	
-	class Kundensichtfenster extends JPanel{
+	
+	class ArtikelTableModel extends AbstractTableModel{
+
+		private String[] columnNames = {"Artikelnummer","Bezeichnung","Preis","Packungsgröße","Bestand"};
+		private Vector<Vector<Object>> data = new Vector<>();
 		
-		JPanel overviewButtons = new JPanel();
-		JButton alleButton = new JButton("Alle");
-		JButton sortNrButton = new JButton("Sort. Nr.");
-		JButton sortBezButton = new JButton("Sort. Name");
-		JButton sucheButton = new JButton("Suche");
-		JTextField sucheField = new JTextField();
+		public ArtikelTableModel(Vector<Artikel> liste) {
+
+			for(Artikel art : liste){
+				
+				Vector<Object> tmp = new Vector<>();
+				
+				tmp.addElement(art.getArtikelnummer());
+				tmp.addElement(art.getBezeichnung());
+				tmp.addElement(art.getPreis());
+				tmp.addElement(((Massengutartikel)art).getPackungsgroesse());
+				tmp.addElement(art.getBestand());
+				
+				data.addElement(tmp);
+				
+			}
+
+		}
+
+		@Override
+		public int getColumnCount() {
+			return columnNames.length;
+		}
+
+		@Override
+		public int getRowCount() {
+			return data.size();
+		}
+
+		@Override
+		public Object getValueAt(int arg0, int arg1) {
+			return data.elementAt(arg0).elementAt(arg1);
+		}
 		
-		JPanel leftAreaActionField = new JPanel();
-		JTextArea auflistung = new JTextArea();
-		JButton aktion = new JButton("Bearbeiten");
+		
+		
+	}
+	
+	class Artikelsichtfenster extends Sichtfenster{
+
+		public Artikelsichtfenster() throws AccessRestrictedException{
+			super(new ArtikelTableModel(eShop.alleArtikelAusgeben(user)),"In Warenkorb", true);		
+		}
+				
+		@Override
+		public void auflistungInitialize() {
+			//auflistung.set
+		}
+
+		@Override
+		public void sortiereNummer() {
+			/*
+			Vector<Artikel> artSort;
+			try {
+				artSort = eShop.alleArtikelAusgeben(user);
+				//Sortieren nach Artikelnummer
+				Collections.sort(artSort, 
+					(Artikel o1, Artikel o2) -> o1.getArtikelnummer() - o2.getArtikelnummer());
+				auflistung.setText("");
+				
+				for (Artikel art : artSort){
+					auflistung.append(art.toString() + "\n");
+				}
+			} catch (AccessRestrictedException e) {
+				auflistung.setText(e.getMessage());
+			}
+			*/
+		}
+
+		@Override
+		public void sortiereBezeichnung() {
+			/*
+			Vector<Artikel> artSort;
+			try {
+				artSort = eShop.alleArtikelAusgeben(user);
+				//Sortieren nach Artikelnummer
+				Collections.sort(artSort, 
+					(Artikel o1, Artikel o2) -> o1.getBezeichnung().compareTo(o2.getBezeichnung()));
+				auflistung.setText("");
+				for (Artikel art : artSort){
+					auflistung.append(art.toString() + "\n");
+				}
+			} catch (AccessRestrictedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			*/
+		}
+
+		@Override
+		public void suche(String suchStr) {
+			/*
+			try {
+				int suchInt = Integer.parseInt(suchStr);
+				try {
+					Artikel art = eShop.artikelSuchen(suchInt, user);
+					auflistung.setText(art.toString());
+				} catch (ArticleNonexistantException ane) {
+					// TODO Exception-Handling
+				} catch (AccessRestrictedException are){
+					// TODO Exception-Handling
+				}
+					
+			} catch (NumberFormatException e) {
+				try {
+					Vector<Artikel> art = eShop.artikelSuchen(suchStr, user);
+					auflistung.setText("");
+					for (Artikel artikel : art) {
+						auflistung.append(artikel.toString() + "\n");
+					}
+				} catch (ArticleNonexistantException ane) {
+					// TODO Exception-Handling
+				} catch (AccessRestrictedException are){
+					// TODO Exception-Handling
+				}
+			}
+			*/
+		}
+	}
+	
+	class Kundensichtfenster extends Sichtfenster{
 		
 		public Kundensichtfenster(){
+			super(null,"Bearbeiten", false);
+		}
+
+		@Override
+		public void auflistungInitialize() {
+			/*
+			try {
+				auflistung.setText("");
+				for (Kunde ku : eShop.alleKundenAusgeben(user)){
+					auflistung.append(ku.toString() + "\n");
+				}
+			} catch (AccessRestrictedException e) {
+				auflistung.setText(e.getMessage());
+			}	
+			*/	
+		}
+
+		@Override
+		public void sortiereNummer() {
+//			Vector<Kunde> kuSort;
+//			try {
+//				kuSort = eShop.alleKundenAusgeben(user);
+//				//Sortieren nach Artikelnummer
+//				Collections.sort(kuSort, 
+//					(Kunde o1, Kunde o2) -> o1.getId() - o2.getId());
+//				auflistung.setText("");
+//				for (Kunde ku : kuSort){
+//					auflistung.append(ku.toString() + "\n");
+//				}
+//			} catch (AccessRestrictedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+		}
+
+		@Override
+		public void sortiereBezeichnung() {
+			// TODO Auto-generated method stub
 			
-			this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-			this.setPreferredSize(new Dimension((int)(GUI.this.getWidth()*0.55), (int)(GUI.this.getHeight()*0.8)));
-			
-			overviewButtons.setMaximumSize(new Dimension(1024, 40));
-			
-			this.add(overviewButtons);
-			this.add(auflistung);
-			this.add(leftAreaActionField);
-			
-	//		alleButton.setSize((int) (overviewButtons.getSize().getWidth()*0.15), 30);
-	//		sortNrButton.setSize((int) (overviewButtons.getSize().getWidth()*0.15), 30);
-	//		sortBezButton.setSize((int) (overviewButtons.getSize().getWidth()*0.15), 30);
-	//		sucheButton.setSize((int) (overviewButtons.getSize().getWidth()*0.15), 30);
-	//		sucheField.setSize((int) (overviewButtons.getSize().getWidth()*0.2), 30);
-			
-			overviewButtons.setLayout(new BoxLayout(overviewButtons, BoxLayout.X_AXIS));
-			overviewButtons.add(alleButton);
-			overviewButtons.add(sortNrButton);
-			overviewButtons.add(sortBezButton);
-			overviewButtons.add(sucheField);
-			overviewButtons.add(sucheButton);
-			overviewButtons.setVisible(true);
-			
-			auflistung.setEditable(false);
-			
-			leftAreaActionField.add(aktion);
-			
+		}
+
+		@Override
+		public void suche(String suchStr) {
+			// TODO Auto-generated method stub
 			
 		}
 	}
 	
-	class Mitarbeitersichtfenster extends JPanel{
-		
-		JPanel overviewButtons = new JPanel();
-		JButton alleButton = new JButton("Alle");
-		JButton sortNrButton = new JButton("Sort. Nr.");
-		JButton sortBezButton = new JButton("Sort. Name");
-		JButton sucheButton = new JButton("Suche");
-		JTextField sucheField = new JTextField();
-		
-		JPanel leftAreaActionField = new JPanel();
-		JTextArea auflistung = new JTextArea();
-		JButton aktion = new JButton("Bearbeiten");
-		
+	class Mitarbeitersichtfenster extends Sichtfenster{
+
 		public Mitarbeitersichtfenster(){
+			super(null,"Bearbeiten", false);
+		}
+
+		@Override
+		public void auflistungInitialize() {
+			// TODO Auto-generated method stub
 			
-			this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-			this.setPreferredSize(new Dimension((int)(GUI.this.getWidth()*0.55), (int)(GUI.this.getHeight()*0.8)));
+		}
+
+		@Override
+		public void sortiereNummer() {
+//			Vector<Mitarbeiter> maSort;
+//			try {
+//				maSort = eShop.alleMitarbeiterAusgeben(user);
+//				//Sortieren nach Artikelnummer
+//				Collections.sort(maSort, 
+//					(Mitarbeiter o1, Mitarbeiter o2) -> o1.getId() - o2.getId());
+//				auflistung.setText("");
+//				for (Mitarbeiter ma : maSort){
+//					auflistung.append(ma.toString() + "\n");
+//				}
+//			} catch (AccessRestrictedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+		}
+
+		@Override
+		public void sortiereBezeichnung() {
+			// TODO Auto-generated method stub
 			
-			overviewButtons.setMaximumSize(new Dimension(1024, 40));
+		}
+
+		@Override
+		public void suche(String suchStr) {
+			// TODO Auto-generated method stub
 			
-			this.add(overviewButtons);
-			this.add(auflistung);
-			this.add(leftAreaActionField);
-			
-	//		alleButton.setSize((int) (overviewButtons.getSize().getWidth()*0.15), 30);
-	//		sortNrButton.setSize((int) (overviewButtons.getSize().getWidth()*0.15), 30);
-	//		sortBezButton.setSize((int) (overviewButtons.getSize().getWidth()*0.15), 30);
-	//		sucheButton.setSize((int) (overviewButtons.getSize().getWidth()*0.15), 30);
-	//		sucheField.setSize((int) (overviewButtons.getSize().getWidth()*0.2), 30);
-			
-			overviewButtons.setLayout(new BoxLayout(overviewButtons, BoxLayout.X_AXIS));
-			overviewButtons.add(alleButton);
-			overviewButtons.add(sortNrButton);
-			overviewButtons.add(sortBezButton);
-			overviewButtons.add(sucheField);
-			overviewButtons.add(sucheButton);
-			overviewButtons.setVisible(true);
-			
-			auflistung.setEditable(false);
-			
-			leftAreaActionField.add(aktion);
 		}
 	}
 	
@@ -346,10 +467,15 @@ public class GUI extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent ae) {
 			if (ae.getSource().equals(artikelButton)){
-				leftArea.remove(leftArea.getComponent(1));
-				leftArea.add(new Artikelsichtfenster());
-				leftArea.revalidate();
-				leftArea.repaint();
+				try {
+					leftArea.remove(leftArea.getComponent(1));
+					leftArea.add(new Artikelsichtfenster());
+					leftArea.revalidate();
+					leftArea.repaint();
+				} catch (AccessRestrictedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			} else if (ae.getSource().equals(kundenButton)) {
 				leftArea.remove(leftArea.getComponent(1));
 				leftArea.add(new Kundensichtfenster());
