02a34c9c1d025b5954f23a2fe826e197123b5cfe
Fabian <fabian.niehaus.94@gmail.com>
2017-06-15 14:04:13 +0200

    Auslagern / Cleanup (unfished)

--- organorkanorca/src/user/GUI.java
+++ organorkanorca/src/user/GUI.java
@@ -1,13 +1,30 @@
 package user;
 
-import javax.swing.*;
-import javax.swing.event.RowSorterEvent;
-import javax.swing.event.RowSorterListener;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Vector;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.RowFilter;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.JTableHeader;
-import javax.swing.table.TableColumn;
 import javax.swing.table.TableRowSorter;
 
 import org.jdesktop.swingx.JXTable;
@@ -15,7 +32,6 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.xy.XYDataset;
 
 import data_objects.Artikel;
 import data_objects.Ereignis;
@@ -25,7 +41,6 @@ import data_objects.Mitarbeiter;
 import data_objects.Person;
 import data_objects.Rechnung;
 import data_objects.Warenkorb;
-import domain.Ereignisverwaltung;
 import domain.eShopCore;
 import domain.exceptions.AccessRestrictedException;
 import domain.exceptions.ArticleAlreadyInBasketException;
@@ -34,28 +49,13 @@ import domain.exceptions.ArticleStockNotSufficientException;
 import domain.exceptions.BasketNonexistantException;
 import domain.exceptions.InvalidAmountException;
 import domain.exceptions.InvalidPersonDataException;
-import domain.exceptions.LoginFailedException;
 import domain.exceptions.MaxIDsException;
 import domain.exceptions.PersonNonexistantException;
 import net.miginfocom.swing.MigLayout;
 //import user.GUI.MainWindow.eShopTableModel;
+import user.LoginWindow.LoginListener;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Vector;
-
-public class GUI {
+public class GUI implements LoginListener {
 	
 	private eShopCore eShop;
 	private Person user;
@@ -68,7 +68,7 @@ public class GUI {
 			
 			eShop = new eShopCore();
 			
-			loginwindow = new LoginWindow("OrganOrkanOrca eShop");
+			loginwindow = new LoginWindow("OrganOrkanOrca eShop", eShop, this);
 			
 		} catch (IOException | ArticleNonexistantException | PersonNonexistantException e1) {
 			
@@ -79,185 +79,7 @@ public class GUI {
 		
 	}
 	
-	private class LoginButtonListener implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			try {
-				user = eShop.anmelden(loginwindow.benutzerIdAuslesen(), loginwindow.passwortAuslesen());
-				mainwindow = new MainWindow("OrganOrkanOrca eShop");
-				loginwindow.dispose();
-			} catch (NumberFormatException | LoginFailedException e1) {
-				JOptionPane.showMessageDialog(loginwindow, "Anmeldung fehlgeschlagen");
-			}
-		}
-	}
 	
-	private class LoginUserUmgehenListener implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			try {
-				user = eShop.anmelden(1001, "test");
-			} catch (LoginFailedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			mainwindow = new MainWindow("OrganOrkanOrca eShop");
-			loginwindow.dispose();
-			
-		}
-		
-	}
-	
-	private class LoginMitarbeiterUmgehenListener implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			try {
-				user = eShop.anmelden(9000, "test2");
-			} catch (LoginFailedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			mainwindow = new MainWindow("OrganOrkanOrca eShop");
-			loginwindow.dispose();
-		}
-	}
-	
-	private class LoginNeuerNutzerAnlegenListener implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			
-			JTextField firstnameField = new JTextField();
-			JTextField lastnameField = new JTextField();
-			JTextField addressStreetField = new JTextField();
-			JTextField addressTownField = new JTextField();
-			JTextField addressZipField = new JTextField();			
-			JTextField passwordField = new JPasswordField();
-			
-			Object[] message = {
-			    "Vorname:", firstnameField,
-			    "Nachname:", lastnameField,
-			    "Straße:", addressStreetField,
-			    "Stadt:", addressTownField,
-			    "PLZ:", addressZipField,
-			    "Passwort:", passwordField
-			};
-
-			int option = JOptionPane.showConfirmDialog(null, message, "Nutzer anlegen", JOptionPane.OK_CANCEL_OPTION);
-			if (option == JOptionPane.OK_OPTION) {
-			   if (!firstnameField.getText().equals("")){
-				   if (!lastnameField.getText().equals("")){
-					   if (!addressStreetField.getText().equals("")){
-						   if (!addressTownField.getText().equals("")){
-							   if (!addressZipField.getText().equals("") && addressZipField.getText().length() == 5){
-								   if (!passwordField.getText().equals("")){
-									   try {
-										   
-											Kunde ku = eShop.erstelleKunde(firstnameField.getText(), 
-														   lastnameField.getText(), 
-														   passwordField.getText(), 
-														   addressStreetField.getText(), 
-														   addressZipField.getText(), 
-														   addressTownField.getText(), 
-														   null);
-											
-											JOptionPane.showMessageDialog(loginwindow, "Benutzer " + ku.getId() + " erfolgreich erstellt");
-											
-										} catch (MaxIDsException e1) {
-											JOptionPane.showMessageDialog(loginwindow, e1.getMessage());
-										} catch (AccessRestrictedException e1) {
-											JOptionPane.showMessageDialog(loginwindow, e1.getMessage());
-										} catch (InvalidPersonDataException e1) {
-											JOptionPane.showMessageDialog(loginwindow, e1.getMessage());
-										}
-								   }
-							   }
-						   }
-					   }
-				   }
-			   } else {
-				   JOptionPane.showMessageDialog(loginwindow, "Benutzer erstellen fehlgeschlagen");
-			   }
-			} else {
-				JOptionPane.showMessageDialog(loginwindow, "Benutzer erstellen abgebrochen");
-			}
-			
-		}
-	}
-	
-	private class LoginWindow extends JFrame {
-		
-		JTabbedPane tabbedPane = new JTabbedPane();
-		
-		JLabel benutzerLabel = new JLabel("Benutzer");
-		JLabel passwortLabel = new JLabel("Passwort");
-		
-		JTextField benutzerField = new JTextField(10);
-		JPasswordField passwortField = new JPasswordField(10);
-		
-		JLabel headerLabel = new JLabel("Willkommen bei OrganOrkanOrca");
-		
-		JButton anmeldenButton = new JButton("Login");
-		JButton registrierenButton = new JButton("Registrieren");
-		
-		public LoginWindow(String titel){
-			super(titel);
-			
-			JPanel form = new JPanel();
-			
-			form.setLayout(new MigLayout());
-			
-			form.add(headerLabel, "span 2, wrap");
-			form.add(benutzerLabel);
-			form.add(benutzerField, "wrap");
-			form.add(passwortLabel);
-			form.add(passwortField, "wrap");
-			form.add(anmeldenButton);
-			form.add(registrierenButton);
-			
-			JPanel pU = new JPanel();
-			JButton bU = new JButton("Als User anmelden");
-			bU.addActionListener(new LoginUserUmgehenListener());
-			pU.add(bU);
-			
-			JPanel pM = new JPanel();
-			JButton bM = new JButton("Als Mitarbeiter anmelden");
-			bM.addActionListener(new LoginMitarbeiterUmgehenListener());
-			pM.add(bM);
-			
-			tabbedPane.addTab("Anmelden", null, form, "Anmeldung");
-			tabbedPane.addTab("User", null, pU, "Shortcut User");
-			tabbedPane.addTab("Mitarbeiter", null, pM, "Shortcut Mitarbeiter");
-			
-			tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
-			
-			this.getContentPane().add(tabbedPane);
-			
-			anmeldenButton.addActionListener(new LoginButtonListener());
-			registrierenButton.addActionListener(new LoginNeuerNutzerAnlegenListener());
-			
-			this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			
-			this.setLocationRelativeTo(null);
-			
-			this.pack();
-			
-			this.setVisible(true);
-		}
-		
-		public int benutzerIdAuslesen() throws NumberFormatException{
-			return Integer.parseInt(benutzerField.getText());
-		}
-		
-		public String passwortAuslesen(){
-			char[] pw = passwortField.getPassword();
-			passwortField.setText("");
-			return new String(pw);
-		}
-	}
 	
 	private class MainWindow extends JFrame {
 		
@@ -1786,7 +1608,8 @@ public class GUI {
 					
 				} else if(ae.getSource().equals(logoutButton)){
 					mainwindow.dispose();
-					loginwindow = new LoginWindow("OrganOrkanOrca eShop");
+					loginwindow = new LoginWindow("OrganOrkanOrca eShop", eShop, GUI.this);
+					//TODO Auslagern
 				}
 				
 			}
@@ -1796,5 +1619,18 @@ public class GUI {
 
 	public static void main(String[] args){
 		GUI gui = new GUI();
+	}
+
+	@Override
+	public void userLoggedIn(Person user) {
+		this.user = user;
+		mainwindow = new MainWindow("OrganOrkanOrca eShop");
+		loginwindow.dispose();		
+	}
+
+	@Override
+	public void loginCancelled() {
+		// TODO Auto-generated method stub
+		
 	}
 }
